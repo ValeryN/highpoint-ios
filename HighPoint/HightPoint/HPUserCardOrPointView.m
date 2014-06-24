adab4bd9937661e1a53d962f81e7536a2cfe1cde
teremalex <mterekhov@outlook.com>
2014-06-24 17:28:33 +0400

    TASK282: fixing constraint for 3,5''

--- HighPoint/HightPoint/HPUserCardOrPointView.m
+++ HighPoint/HightPoint/HPUserCardOrPointView.m
@@ -9,6 +9,11 @@
 //==============================================================================
 
 #import "HPUserCardOrPointView.h"
+#import "UIDevice+HighPoint.h"
+
+//==============================================================================
+
+#define CONSTRAINT_HEIGHT_FOR_BGIMAGE 416
 
 //==============================================================================
 
@@ -42,7 +47,16 @@
     else
         _childContainerView = [cardOrPoint userPointWithDelegate: delegate];
 
+    [self fixUserCardConstraint];
+
     [self addSubview: _childContainerView];
+    [self fixFrame];
+}
+
+//==============================================================================
+
+- (void) fixFrame
+{
     CGRect rect = _childContainerView.frame;
     rect.origin.x = 0;
     rect.origin.y = 0;
@@ -51,6 +65,23 @@
     rect = self.frame;
     rect.size = _childContainerView.frame.size;
     self.frame = rect;
+}
+
+//==============================================================================
+
+- (void) fixUserCardConstraint
+{
+    if ((![UIDevice hp_isWideScreen]) &&
+        ([_childContainerView isKindOfClass:[HPUserCardView class]]))
+    {
+        HPUserCardView* v = (HPUserCardView*)_childContainerView;
+        NSArray* cons = v.backgroundAvatar.constraints;
+        for (NSLayoutConstraint* consIter in cons)
+        {
+            if (consIter.firstAttribute == NSLayoutAttributeHeight)
+                consIter.constant = CONSTRAINT_HEIGHT_FOR_BGIMAGE;
+        }
+    }
 }
 
 //==============================================================================
