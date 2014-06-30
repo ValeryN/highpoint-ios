2269267131b0fda04d12c9dd2dc96d373fa5cc58
teremalex <mterekhov@outlook.com>
2014-06-30 15:38:33 +0400

    TASK282: adding point view UI elements

--- HighPoint/HightPoint/HPUserPointView.h
+++ HighPoint/HightPoint/HPUserPointView.h
@@ -19,10 +19,11 @@
 
 @property (nonatomic, weak) NSObject<UserCardOrPointProtocol>* delegate;
 
-@property (nonatomic, weak) IBOutlet HPAvatarView* avatar;
+@property (nonatomic, weak) HPAvatarView* avatar;
 @property (nonatomic, weak) IBOutlet UIImageView* backgroundAvatar;
 @property (nonatomic, weak) IBOutlet UILabel* details;
 @property (nonatomic, weak) IBOutlet UILabel* name;
+@property (nonatomic, weak) IBOutlet UIView* avatarGroup;
 
 - (void) initObjects;
 - (IBAction) pointButtonPressed: (id)sender;
