37db7ac0c433f3b8be303f3b11dd422789304f6b
Julia Pozdnyakova <juliapozdnyakova@Mac-mini-Julia-2.local>
2014-07-10 13:00:48 +0400

    #11321 current user point and card controller added

---
+++ HighPoint/HightPoint/HPCurrentUserCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  HPCurrentUserCardViewController.h
+//  HighPoint
+//
+//  Created by Julia Pozdnyakova on 10.07.14.
+//  Copyright (c) 2014 SurfStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPCurrentUserCardViewController : UIViewController
+
+@end
