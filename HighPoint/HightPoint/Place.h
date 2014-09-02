cf9cf8d7757521d82b2be8709defa7fcaf104763
AndreyAnAnisimov <andrey.anisimov@surfstudio.ru>
2014-09-02 16:18:21 +0400

    #12992 change reference  request for place & city link

--- HighPoint/HightPoint/Place.h
+++ HighPoint/HightPoint/Place.h
@@ -2,20 +2,21 @@
 //  Place.h
 //  HighPoint
 //
-//  Created by Julia Pozdnyakova on 18.07.14.
+//  Created by Andrey Anisimov on 02.09.14.
 //  Copyright (c) 2014 SurfStudio. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
-@class User;
+@class City, User;
 
 @interface Place : NSManagedObject
 
-@property (nonatomic, retain) NSNumber * id_;
 @property (nonatomic, retain) NSNumber * cityId;
+@property (nonatomic, retain) NSNumber * id_;
 @property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) User *user;
+@property (nonatomic, retain) City *city;
 
 @end
