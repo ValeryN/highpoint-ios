f3ab2aed26478f32cbc9a818ee5b89ab6c76d2cf
Eugene Antropov <antigp@gmail.com>
2014-09-03 14:34:18 +0400

    AddCareer view controller

---
+++ HighPoint/HightPoint/HPAddCareerViewController.h
@@ -0,0 +1,13 @@
+//
+//  HPAddEducationViewController.h
+//  HighPoint
+//
+//  Created by Andrey Anisimov on 04.08.14.
+//  Copyright (c) 2014 SurfStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPAddCareerViewController : UIViewController
+@property (nonatomic, retain) RACSubject *returnSignal;
+@end
