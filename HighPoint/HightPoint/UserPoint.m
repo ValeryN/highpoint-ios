8e5ad6788b1b08eef32fcff66271785ed2c9be00
Julia Pozdnyakova <julia.pozdnyakova@surfstudio.ru>
2014-09-05 13:44:40 +0400

    #13029 add user point files

--- HighPoint/HightPoint/UserPoint.m
+++ HighPoint/HightPoint/UserPoint.m
@@ -2,11 +2,12 @@
 //  UserPoint.m
 //  HighPoint
 //
-//  Created by Andrey Anisimov on 26.06.14.
+//  Created by Julia Pozdnyakova on 05.09.14.
 //  Copyright (c) 2014 SurfStudio. All rights reserved.
 //
 
 #import "UserPoint.h"
+#import "User.h"
 
 
 @implementation UserPoint
@@ -18,5 +19,6 @@
 @dynamic pointUserId;
 @dynamic pointValidTo;
 @dynamic likedBy;
+@dynamic user;
 
 @end
