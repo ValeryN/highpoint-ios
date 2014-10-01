4229619d23ed77b085bb887b0265a2b9f921120e
Eugene Antropov <antigp@gmail.com>
2014-10-01 15:26:38 +0400

    User request cells text, getting user not work

--- HighPoint/HightPoint/HPChatMsgOpenDenyCell.m
+++ HighPoint/HightPoint/HPChatMsgOpenDenyCell.m
@@ -4,13 +4,39 @@
 //
 
 #import "HPChatMsgOpenDenyCell.h"
+#import "Message.h"
+#import "DataStorage.h"
 
+@interface HPChatMsgOpenDenyCell ()
+@property (nonatomic, weak) IBOutlet UILabel * cellTextLabel;
+@property(nonatomic, weak) Message *message;
+@end
 
-@implementation HPChatMsgOpenDenyCell {
+@implementation HPChatMsgOpenDenyCell
 
+- (void)awakeFromNib {
+    NSNumber *currentUserId = [[DataStorage sharedDataStorage] getCurrentUser].userId;
+    RAC(self, cellTextLabel.text) = [RACObserve(self, message) map:^id(Message *value) {
+        if ([value.destinationId isEqual:currentUserId]) {
+            return [value.chat.user.gender isEqual:@(UserGenderFemale)] ? @"Она отказала открывать вам свое имя и фотографии" : @"Он отказал открывать вам свое имя и фотографии";
+        }
+        else {
+            NSString *name;
+            if ([value.chat.user.visibility isEqual:@(UserVisibilityVisible)]) {
+                name = value.chat.user.name;
+            }
+            else {
+                name = [value.chat.user.gender isEqual:@(UserGenderFemale)] ? @"ей" : @"ему";
+            }
+
+            return [NSString stringWithFormat:@"Вы отказали открывать %@ свое имя и фотографии", name];
+        }
+        return nil;
+    }];
 }
-- (void)bindViewModel:(id)viewModel {
 
+- (void)bindViewModel:(Message *)viewModel {
+    self.message = viewModel;
 }
 
 @end
--- HighPoint/HightPoint/HPChatMsgOpenRequestCell.m
+++ HighPoint/HightPoint/HPChatMsgOpenRequestCell.m
@@ -4,12 +4,38 @@
 //
 
 #import "HPChatMsgOpenRequestCell.h"
+#import "DataStorage.h"
 
+@interface HPChatMsgOpenRequestCell ()
+@property (nonatomic, weak) IBOutlet UILabel * cellTextLabel;
+@property(nonatomic, weak) Message *message;
+@end
 
 @implementation HPChatMsgOpenRequestCell
 
-- (void)bindViewModel:(id)viewModel {
+- (void)awakeFromNib {
+    NSNumber *currentUserId = [[DataStorage sharedDataStorage] getCurrentUser].userId;
+    RAC(self, cellTextLabel.text) = [RACObserve(self, message) map:^id(Message *value) {
+        if ([value.destinationId isEqual:currentUserId]) {
+            NSString *name;
+            if ([value.chat.user.visibility isEqual:@(UserVisibilityVisible)]) {
+                name = value.chat.user.name;
+            }
+            else {
+                name = [value.chat.user.gender isEqual:@(UserGenderFemale)] ? @"Она" : @"Он";
+            }
 
+            return [NSString stringWithFormat:@"%@ просит вас открывать свой профиль. Вы можете открытся ей или начать общаться прямо сейчас.", name];
+        }
+        else {
+            return [value.chat.user.gender isEqual:@(UserGenderFemale)] ? @"Вы попросили открыть её имя и фотографии для вас. Когда она откроется вы получите уведомление." : @"Вы попросили открыть его имя и фотографии для вас. Когда она откроется вы получите уведомление.";
+        }
+        return nil;
+    }];
+}
+
+- (void)bindViewModel:(Message *)viewModel {
+    self.message = viewModel;
 }
 
 @end
