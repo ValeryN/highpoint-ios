0d5c9aaab1921f587ac64d3234d74e8aa154e3f8
Julia Pozdnyakova <julia.pozdnyakova@surfstudio.ru>
2014-10-21 09:40:16 +0400

    #14080 photo album screen new design

---
+++ HighPoint/HightPoint/HPSegmentControl.h
@@ -0,0 +1,15 @@
+//
+//  HPSegmentControl.h
+//  HighPoint
+//
+//  Created by Julia Pozdnyakova on 21.10.14.
+//  Copyright (c) 2014 SurfStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+IB_DESIGNABLE
+@interface HPSegmentControl : UISegmentedControl
+
+@end
