769e67ceb2f667e4e9bd9c7d4be88e0537517ec0
Julia Pozdnyakova <julia.pozdnyakova@surfstudio.ru>
2014-10-22 13:46:36 +0400

    #14081 rounded button pressed state

--- HighPoint/HightPoint/HPRoundButton.m
+++ HighPoint/HightPoint/HPRoundButton.m
@@ -18,6 +18,18 @@
     self.layer.borderWidth = 1;
     self.layer.borderColor = [UIColor colorWithRed:0.f/255.f green:203.f/255.f blue:254.f/255.f alpha:1.f].CGColor;
     self.titleEdgeInsets = UIEdgeInsetsMake(0, 10.f, 0, 10.f);
+    self.layer.masksToBounds = YES;
+}
+
+- (void)setHighlighted:(BOOL)highlighted {
+    [super setHighlighted:highlighted];
+    
+    if (highlighted) {
+        self.backgroundColor = [UIColor colorWithRed:0.f/255.f green:203.f/255.f blue:254.f/255.f alpha:0.15];
+    } else {
+        self.backgroundColor = [UIColor clearColor];
+    }
+    
 }
 
 @end
